c9f33ff901f054f47424e030faae97a6bb0c3415
Lisa <lisacjheinhold@gmail.com>
2023-10-05 20:49:55 -0600

    fin authentication

--- routes/departments.js
+++ routes/departments.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const departmentsController = require('../controllers/departments');
 const { departmentValidationRules, validate } = require('../validator');
+const { isAuthenticated } = require("../authenticate");
 
 router.get('/', departmentsController.getAllDepartments);
 router.get('/:id', departmentsController.getOneDepartment);
-router.post('/', departmentValidationRules(), departmentsController.createDepartment);
-router.put('/:id', departmentValidationRules(), departmentsController.updateDepartment);
-router.delete('/:id', departmentsController.deleteDepartment);
+router.post('/', isAuthenticated, departmentValidationRules(), departmentsController.createDepartment);
+router.put('/:id', isAuthenticated, departmentValidationRules(), departmentsController.updateDepartment);
+router.delete('/:id', isAuthenticated, departmentsController.deleteDepartment);
 
 module.exports = router;
--- routes/employees.js
+++ routes/employees.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const employeeController = require('../controllers/employees');
 const { employeeValidationRules, validate } = require('../validator');
+const { isAuthenticated } = require("../authenticate");
 
 router.get('/', employeeController.getAllEmployees);
 router.get('/:id', employeeController.getOneEmployee);
-router.post('/', employeeValidationRules(), employeeController.createEmployee);
-router.put('/:id', employeeValidationRules(), employeeController.updateEmployee);
-router.delete('/:id', employeeController.deleteEmployee);
+router.post('/', isAuthenticated, employeeValidationRules(), employeeController.createEmployee);
+router.put('/:id',isAuthenticated,  employeeValidationRules(), employeeController.updateEmployee);
+router.delete('/:id', isAuthenticated, employeeController.deleteEmployee);
 
 module.exports = router;
